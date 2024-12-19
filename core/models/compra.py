e69af2cfcfee9e7d95ca708364213d811d45b830
gabrielgomes14 <gomesgalikosky@gmail.com>
2024-12-19 10:40:31 -0300

    Adicionando ordenação

--- core/models/compra.py
+++ core/models/compra.py
@@ -1,7 +1,7 @@
 from django.db import models
-
 from .user import User
 from .livro import Livro
+
 
 class Compra(models.Model):
     class StatusCompra(models.IntegerChoices):
@@ -11,15 +11,19 @@ class Compra(models.Model):
         ENTREGUE = 4, "Entregue"
 
     usuario = models.ForeignKey(User, on_delete=models.PROTECT, related_name="compras")
-    status = models.IntegerField(choices=StatusCompra.choices,  default=StatusCompra.CARRINHO)
-    data = models.DateTimeField(auto_now_add=True)
-
+    status = models.IntegerField(choices=StatusCompra.choices, default=StatusCompra.CARRINHO)
+    
     @property
     def total(self):
-        return sum(item.preco * item.quantidade for item in self.itens.all())
+        # total = 0
+        # for item in self.itens.all():
+        #     total += item.livro.preco * item.quantidade
+        # return total
+        return sum(item.livro.preco * item.quantidade for item in self.itens.all())
+
 
 class ItensCompra(models.Model):
     compra = models.ForeignKey(Compra, on_delete=models.CASCADE, related_name="itens")
     livro = models.ForeignKey(Livro, on_delete=models.PROTECT, related_name="+")
-    quantidade = models.IntegerField(default=1)
-    preco = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    quantidade = models.PositiveIntegerField(default=1)
+
