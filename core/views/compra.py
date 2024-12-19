cb5738c41e0446c1bc7645586e2a2327da00f068
gabrielgomes14 <gomesgalikosky@gmail.com>
2024-12-18 21:08:26 -0300

    Filtrando apenas as compras do usuário autenticado

--- core/views/compra.py
+++ core/views/compra.py
@@ -1,11 +1,19 @@
 from rest_framework.viewsets import ModelViewSet
-from core.models import Compra
-from core.serializers import CompraSerializer, CompraCreateUpdateSerializer, CompraListSerializer, CompraSerializer
 
+from core.models import Compra
+from core.serializers import CompraCreateUpdateSerializer, CompraListSerializer, CompraSerializer
 
 class CompraViewSet(ModelViewSet):
     queryset = Compra.objects.all()
     serializer_class = CompraSerializer
+
+    def get_queryset(self):
+        usuario = self.request.user
+        if usuario.is_superuser:
+            return Compra.objects.all()
+        if usuario.groups.filter(name="Administradores"):
+            return Compra.objects.all()
+        return Compra.objects.filter(usuario=usuario)
 
     def get_serializer_class(self):
         if self.action == "list":
